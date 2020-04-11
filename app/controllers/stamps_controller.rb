e73c07d0d7478a2f0e942763b00bb19821fd07a3
masa0150 <masa98989801@yahoo.co.jp>
2020-04-11 16:54:22 +0900

    スタンプ一覧　each文で表示

--- app/controllers/stamps_controller.rb
+++ app/controllers/stamps_controller.rb
@@ -1,7 +1,5 @@
 class StampsController < ApplicationController
   def index
-    @stamp = Stamp.find(2)
-    @stamp2 = Stamp.find(3)
-    @stamp3 = Stamp.find(4)
+    @stamps = Stamp.all
   end
 end
--- app/models/stamp.rb
+++ app/models/stamp.rb
@@ -1,6 +1,3 @@
 class Stamp < ApplicationRecord
   has_many :post
-  def index 
-    @stamps = Stamp.select("image")
-  end
 end
